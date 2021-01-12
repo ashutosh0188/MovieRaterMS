155066ccd80116ad3cc66cd6f2090e831d3f6f25
ashutosh0188 <ashutosh0188@bitbucket.org>
2021-01-12 12:18:12 +0530

    Created MovieCatalogService

---
+++ movie-catalog-services/src/test/java/movierater/moviecatalogservices/MovieCatalogServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package movierater.moviecatalogservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
