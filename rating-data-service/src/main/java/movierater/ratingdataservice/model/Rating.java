28f9a2a7ffb11763d2a3596995f4415cfd860050
ashutosh0188 <ashutosh0188@bitbucket.org>
2021-01-12 16:38:59 +0530

    Created RatingDataController and Rating model class

--- rating-data-service/src/main/java/movierater/ratingdataservice/model/Rating.java
+++ rating-data-service/src/main/java/movierater/ratingdataservice/model/Rating.java
@@ -1,4 +1,27 @@
 package movierater.ratingdataservice.model;
 
 public class Rating {
+    private String movieId;
+    private double rating;
+
+    public Rating(String movieId, double rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
