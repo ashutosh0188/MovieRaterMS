cba2325f4d48c32b4a14ec507947d9cd2cb3c47b
ashutosh0188 <ashutosh0188@bitbucket.org>
2021-01-13 20:44:03 +0530

    Added eureka client dependency to movie-catalog-services to make this application act as eureka client and register itself over eureka discovery-server

--- movie-catalog-services/src/main/java/movierater/moviecatalogservices/MovieCatalogServicesApplication.java
+++ movie-catalog-services/src/main/java/movierater/moviecatalogservices/MovieCatalogServicesApplication.java
@@ -2,8 +2,10 @@ package movierater.moviecatalogservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //This is optional. After adding eureka client dependency. This SpringBootApplication should behave as eureka client by default.
 public class MovieCatalogServicesApplication {
 
 	public static void main(String[] args) {
