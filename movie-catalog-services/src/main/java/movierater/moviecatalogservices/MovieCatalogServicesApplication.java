84aa90c88a85401b2b5120a09840bc021cae095c
ashutosh0188 <ashutosh0188@bitbucket.org>
2021-01-21 22:50:15 +0530

    Added hystrix dependency and added default fallback method mechanism. Downgraded springboot to 2.3.7 to add hystrix dependency

--- movie-catalog-services/src/main/java/movierater/moviecatalogservices/MovieCatalogServicesApplication.java
+++ movie-catalog-services/src/main/java/movierater/moviecatalogservices/MovieCatalogServicesApplication.java
@@ -2,10 +2,12 @@ package movierater.moviecatalogservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient //This is optional. After adding eureka client dependency. This SpringBootApplication should behave as eureka client by default.
+@EnableCircuitBreaker
 public class MovieCatalogServicesApplication {
 
 	public static void main(String[] args) {
--- movie-catalog-services/src/main/java/movierater/moviecatalogservices/controller/MovieCatalogController.java
+++ movie-catalog-services/src/main/java/movierater/moviecatalogservices/controller/MovieCatalogController.java
@@ -1,8 +1,8 @@
 package movierater.moviecatalogservices.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import movierater.moviecatalogservices.model.CatalogItem;
 import movierater.moviecatalogservices.model.Movie;
-import movierater.moviecatalogservices.model.Rating;
 import movierater.moviecatalogservices.model.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,6 +23,7 @@ public class MovieCatalogController {
     private RestTemplate restTemplate;
 
     @GetMapping("/items/{userId}")
+    @HystrixCommand(fallbackMethod = "catalogItemFromCache")
     public List<CatalogItem> catalogItem(@PathVariable String userId) {
         UserRating userRating = restTemplate.getForObject("http://rating-data-service/rating/user/"+userId, UserRating.class);
         List<CatalogItem> res = userRating.getRatings().stream().map(r-> {
@@ -32,5 +32,9 @@ public class MovieCatalogController {
         }).collect(Collectors.toList());
         System.out.println(res);
         return res;
+    }
+
+    public List<CatalogItem> catalogItemFromCache(@PathVariable String userId) {
+        return Arrays.asList(new CatalogItem("No movie", "", 0.0));
     }
 }
